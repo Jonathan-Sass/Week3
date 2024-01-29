baffcbc0689a8952dc963d71c7e8d867c73c219b
Jonathan <jonsasser@gmail.com>
2024-01-28 16:04:40 -0800

    Fizzbuzz was fun

---
+++ FizzBuzz/FizzBuzz.js
@@ -0,0 +1,13 @@
+let i = 1;
+console.log (i);
+for (i = 2; i < 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
